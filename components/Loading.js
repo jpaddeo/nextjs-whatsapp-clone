5e1988b802d9e69485cc1cfa0dba3414b9b46623
jpaddeo <jpaddeo@gmail.com>
2021-07-21 15:46:15 -0300

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,8 @@
-import Image from 'next/image';
-
 const Loading = () => {
   return (
     <center style={{ display: 'gird', placeItems: 'center', height: '100vh' }}>
       <div>
-        <Image
+        <img
           src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png'
           alt='Whatsapp logo'
           style={{ marginBottom: 10 }}
