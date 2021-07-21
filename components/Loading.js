73563d167f6b6651adbe6bae533a5a1210209956
jpaddeo <jpaddeo@gmail.com>
2021-07-21 15:50:03 -0300

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -2,12 +2,6 @@ const Loading = () => {
   return (
     <center style={{ display: 'gird', placeItems: 'center', height: '100vh' }}>
       <div>
-        <img
-          src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png'
-          alt='Whatsapp logo'
-          style={{ marginBottom: 10 }}
-          height={200}
-        />
         <h1>Loading...</h1>
       </div>
     </center>
